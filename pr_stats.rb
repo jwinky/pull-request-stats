a06682c91c0cd96f9a77d583ec3fa811d5f271d2
Justin Wienckowski <justin.wienckowski@chime.com>
2021-04-29 14:53:34 -0700

    Add sample counts

--- pr_stats.rb
+++ pr_stats.rb
@@ -55,10 +55,11 @@ class GitHubPullRequestInfo
 end
 
 SEP = "------------------------------------------------".freeze
-def report(stats, description)
+def report(stats, description, sample_count)
   puts SEP
   puts 'Time To Merge Stats'
   puts description
+  puts "n = #{sample_count}"
   puts SEP
   puts format(' Range: %2d - %2d days', stats.min, stats.max)
   puts format('  Mean: %5.2f', stats.mean)
@@ -83,7 +84,7 @@ def report(stats, description)
 end
 
 records = rows.map {|r| GitHubPullRequestInfo.new(r) }
-sample = records.select { |pr| pr.created > FOUR_MONTHS_AGO } #and (!pr.time_to_merge or pr.time_to_merge < 30) }
+sample = records.select { |pr| pr.created && pr.created > FOUR_MONTHS_AGO } #and (!pr.time_to_merge or pr.time_to_merge < 30) }
 
 def ttm_data(data, minimum_ttm = 0)
   return data.select {|pr|
@@ -93,13 +94,16 @@ end
 
 puts
 
-stats_all = DescriptiveStatistics::Stats.new(ttm_data(sample))
-report(stats_all, 'All Merges (Last 4 months)')
+all_sample = ttm_data(sample)
+stats_all = DescriptiveStatistics::Stats.new(all_sample)
+report(stats_all, 'All Merges (Last 4 months)', all_sample.count)
 
-stats_not_same_day = DescriptiveStatistics::Stats.new(ttm_data(sample, 1))
-report(stats_not_same_day, 'PRs taking >0 days (Last 4 months)')
+nsd_sample = ttm_data(sample, 1)
+stats_not_same_day = DescriptiveStatistics::Stats.new(nsd_sample)
+report(stats_not_same_day, 'PRs taking >0 days (Last 4 months)', nsd_sample.count)
 
-stats_three_days_plus = DescriptiveStatistics::Stats.new(ttm_data(sample, 3))
-report(stats_three_days_plus, 'PRs taking >2 days (Last 4 months)')
+tdp_sample = ttm_data(sample, 3)
+stats_three_days_plus = DescriptiveStatistics::Stats.new(tdp_sample)
+report(stats_three_days_plus, 'PRs taking >2 days (Last 4 months)', tdp_sample.count)
 
 exit 0
